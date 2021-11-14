b3b02de50b463cc24711b56082c6b6406b12eef7
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2021-11-14 12:47:47 +0100

    Update index.js
    updated the delete btn to be on click as against doubleclick

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function() {
+deleteBtn.addEventListener("click", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
